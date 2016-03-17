ae4b9af059934f07061215c770f076e83ef6ede4
wildelf <psizzlepip@yahoo.com>
2016-03-17 16:46:56 -0700

    map identities hooked up

--- vector/MainViewController.swift
+++ vector/MainViewController.swift
@@ -9,89 +9,92 @@
 import UIKit
 import Foundation
 import CoreLocation
+import GoogleMaps
 
 class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+	
 	let locationManager = CLLocationManager()
-
+	
 	@IBOutlet weak var mapView: GMSMapView!
 	
-    @IBOutlet weak var tableView: UITableView!
-    //@IBOutlet weak var mapView: MKMapView! // NOT SURE IF THIS IS CORRECT FOR GOOGLE API
-    
-    @IBAction func onFriends(sender: AnyObject) {
-        // GO TO FRIENDS LIST VIEW CONTROLLER
-    }
-    @IBAction func onCalcPoint(sender: AnyObject) {
-        // GO TO LOCATION DETAILS VIEW
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        tableView.delegate = self
-        tableView.dataSource = self
-			
-			locationManager.delegate = self
-			locationManager.requestWhenInUseAuthorization()
-
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // Replace this with number of friends!
-        return 3
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("MainViewCell", forIndexPath: indexPath) as! MainViewCell
-        
-        // cell.user = users![indexPath.row] // USE SOMETHING LIKE THIS TO GET THE LIST OF FRIENDS DATA
-        
-        return cell
-    }
-    
-
-   /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+	@IBOutlet weak var tableView: UITableView!
+	
+	@IBAction func onFriends(sender: AnyObject) {
+		// GO TO FRIENDS LIST VIEW CONTROLLER
+	}
+	@IBAction func onCalcPoint(sender: AnyObject) {
+		// GO TO LOCATION DETAILS VIEW
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		// Do any additional setup after loading the view.
+		tableView.delegate = self
+		tableView.dataSource = self
+		
+		locationManager.delegate = self
+		locationManager.requestWhenInUseAuthorization()
+		
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		// Replace this with number of friends!
+		return 3
+	}
+	
+	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCellWithIdentifier("MainViewCell", forIndexPath: indexPath) as! MainViewCell
+		
+		// cell.user = users![indexPath.row] // USE SOMETHING LIKE THIS TO GET THE LIST OF FRIENDS DATA
+		
+		return cell
+	}
+	
+	
+	/*
+	// MARK: - Navigation
+	
+	// In a storyboard-based application, you will often want to do a little preparation before navigation
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+	// Get the new view controller using segue.destinationViewController.
+	// Pass the selected object to the new view controller.
+	}
+	*/
+	
 }
 
 extension MainViewController: CLLocationManagerDelegate {
-	// 2
+
+	// called with authorization is granted or revoked
 	func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
-		// 3
+		
+	// checks for location authorization
 		if status == .AuthorizedWhenInUse {
 			
-			// 4
+			// updates live location
 			locationManager.startUpdatingLocation()
 			
-			//5
+			// enables blue loc dot & center on loc button
 			mapView.myLocationEnabled = true
 			mapView.settings.myLocationButton = true
 		}
 	}
 	
-	// 6
+	// called when new location data is received
 	func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
 		if let location = locations.first {
 			
-			// 7
+			// center camera on user location
 			mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 15, bearing: 0, viewingAngle: 0)
 			
-			// 8
+			// turns off location updates
+			// TODO: probably want to set this on a timer
 			locationManager.stopUpdatingLocation()
 		}
 		
--- vector/bridging-header.h
+++ vector/bridging-header.h
@@ -1,13 +1,5 @@
 //
 //  bridging-header.h
 //  vector
-//
-//  Created by Eric Zim on 3/10/16.
-//  Copyright © 2016 WayZimChu. All rights reserved.
-//
 
-#ifndef bridging_header_h
-#define bridging_header_h
 #import <GoogleMaps/GoogleMaps.h>
-
-#endif /* bridging_header_h */
