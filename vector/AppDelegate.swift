0165ffeb635357b63373cf592d3c4cde606ee360
wildelf <psizzlepip@yahoo.com>
2016-03-31 19:45:13 -0700

    updated API key and fixed xib image name

--- vector/AppDelegate.swift
+++ vector/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var storyboard = UIStoryboard(name: "Main", bundle: nil)
 
-    let googleMapsApiKey = "AIzaSyCnIoj_O7MX-MKUI7kGiBuNvmfqt1UtCGo"
+    let googleMapsApiKey = "AIzaSyCMUQTikFRUsS_sudeA_rkaOfM9Vx8AExw"
     let parseApplicationID = "vector"
     let parseBackendKey = "sdf978ys3oh109sdfp939psg9jf803hldf394ls0df3l20sfd9uw"
     let parseServer = "http://mighty-springs-90101.herokuapp.com/parse"
