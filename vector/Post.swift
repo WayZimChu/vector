d5ef34983ab0654fde726239e82e5c54206181e2
r3dcrosse <mrdavidwayman@gmail.com>
2016-04-05 17:17:45 -0700

    Fixed duplicate latitude in backend bug

--- vector/Post.swift
+++ vector/Post.swift
@@ -11,66 +11,6 @@ import Parse
 
 class Post: NSObject {
     
-    /**
-     * Other methods
-     */
-     
-     /**
-     Method to add a user post to Parse (uploading image file)
-     
-     - parameter image: Image that the user wants upload to parse
-     - parameter caption: Caption text input by the user
-     - parameter completion: Block to be executed after save operation is complete
-     */
-//    class func postUserImage(image: UIImage?, withCaption caption: String?, withCompletion completion: PFBooleanResultBlock?) {
-//        // Create Parse object PFObject
-//        let post = PFObject(className: "Post")
-//        
-//        // Add relevant fields to the object
-//        post["media"] = getPFFileFromImage(image) // PFFile column type
-//        post["author"] = PFUser.currentUser() // Pointer column type that points to PFUser
-//        post["caption"] = caption
-//        post["likesCount"] = 0
-//        post["commentsCount"] = 0
-//        post["name"] = PFUser.currentUser()?.username!
-//        
-//        // Save object (following function will save the object in Parse asynchronously)
-//        post.saveInBackgroundWithBlock(completion)
-//    }
-    
-    class func postUserProfile(profile: PFObject, withCompletion completion: PFBooleanResultBlock?) {
-        profile.saveInBackgroundWithBlock(completion)
-    }
-    /**
-     Method to convert UIImage to PFFile
-     
-     - parameter image: Image that the user wants to upload to parse
-     
-     - returns: PFFile for the the data in the image
-     */
-    class func getPFFileFromImage(image: UIImage?) -> PFFile? {
-        // check if image is not nil
-        if let image = image {
-            // get image data and check if that is not nil
-            if let imageData = UIImagePNGRepresentation(image) {
-                return PFFile(name: "image.png", data: imageData)
-            }
-        }
-        return nil
-    }
-    
-    class func resize(image: UIImage, newSize: CGSize) -> UIImage {
-        let resizeImageView = UIImageView(frame: CGRectMake(0, 0, newSize.width, newSize.height))
-        resizeImageView.contentMode = UIViewContentMode.ScaleAspectFill
-        resizeImageView.image = image
-        
-        UIGraphicsBeginImageContext(resizeImageView.frame.size)
-        resizeImageView.layer.renderInContext(UIGraphicsGetCurrentContext()!)
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return newImage
-    }
-    
     class func updateLocation(ID: String, long: Double, lat: Double) {
         let query = PFQuery(className:"Profile")
         query.getObjectInBackgroundWithId("\(ID)") {
@@ -79,7 +19,7 @@ class Post: NSObject {
                 print(error)
             } else if let profileObject = profileObject {
                 profileObject["longitude"] = long
-                profileObject["latitutde"] = lat
+                profileObject["latitude"] = lat
                 profileObject.saveInBackground()
             }
         }
